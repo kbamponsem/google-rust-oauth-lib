c514a4d38c9b54d3c417cecec2106e499b65aed4
Kwabena <kwabenaamponsem@gmail.com>
2023-09-28 18:32:44 +0000

    Makes request to google non-blocking

--- src/lib.rs
+++ src/lib.rs
@@ -29,17 +29,17 @@ impl GoogleOAuthClaims {
 
         return Ok(_claims.claims);
     }
-    fn fetch_google_public_key(key_id: &str) -> Result<String, String> {
+    async fn fetch_google_public_key(key_id: &str) -> Result<String, String> {
         let url = "https://www.googleapis.com/oauth2/v1/certs";
 
         // Send a GET request to the public keys endpoint
-        let response = reqwest::blocking::get(url).unwrap();
+        let response = reqwest::get(url).await.unwrap();
 
         // Check if the request was successful
         let response = response.error_for_status().unwrap();
 
         // Parse the JSON response
-        let json: Value = response.json().unwrap();
+        let json: Value = response.json().await.unwrap();
 
         // Extract the public key for the specified key ID
         let public_key = match json.as_object() {
@@ -70,9 +70,9 @@ impl GoogleOAuthClaims {
 
         key_id.to_string()
     }
-    pub fn decode_jwt(token: &str) -> Result<Self, errors::Error> {
+    pub async fn decode_jwt(token: &str) -> Result<Self, errors::Error> {
         let key_id = Self::get_key_id_from_jwt(token);
-        let pem = Self::fetch_google_public_key(&key_id).unwrap();
+        let pem = Self::fetch_google_public_key(&key_id).await.unwrap();
         println!("PEM: {}", pem);
 
         return Self::verify_google_jwt(token, &pem);
